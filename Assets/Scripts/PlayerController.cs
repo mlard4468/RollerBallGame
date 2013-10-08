363eb83ae47e17a2fa57e7cbe32f89103b5a6772
spmallard <simon.mallard@yahoo.co.uk>
2013-10-08 14:10:34 +0100

    First Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{	
+	
+	public float speed;
+	private int count = 0;
+	public GUIText CountText;
+	public GUIText WinText;
+	
+	void Start()
+	{
+		//count = 0;
+		
+		SetCountText();
+		WinText.text = "";
+	}
+	
+	void FixedUpdate()
+	{
+		float moveVertical = Input.GetAxis ("Vertical");
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		
+		rigidbody.AddForce(new Vector3(moveHorizontal,0.0f,moveVertical)* speed * Time.deltaTime);
+	}
+	
+	void OnTriggerEnter (Collider other) 
+	{
+		if (other.gameObject.tag=="Pickup")
+		{
+			other.gameObject.SetActive(false);
+			count++;
+			SetCountText();
+		}
+		//Destroy(other.gameObject);
+	}
+	
+	void SetCountText()
+	{
+		CountText.text = "Count: "+count.ToString();
+		if (count>=11)
+			WinText.text = "You are a weiner";
+	}
+	
+}
